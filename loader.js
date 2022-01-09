76039086581e176566f87990b9dbb97b83abc9a0
nieznaszmnie <46851262+nieznaszmnie@users.noreply.github.com>
2022-01-09 01:58:10 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/2022-bot/tw2bot.min.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
+javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/2022-bot/tw2overflow.min?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
